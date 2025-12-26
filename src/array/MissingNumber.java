e1c80b5a3a6355f372dd76d1131ddf4dc2ab4176
Mahek Sultana <121860817+mahek522@users.noreply.github.com>
2025-12-26 17:40:19 +0530

    Renamin

--- src/array/MissingNumber.java
+++ src/array/MissingNumber.java
@@ -8,21 +8,21 @@ public class MissingNumber {
 
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Enter the size of array:");
-		int n = scan.nextInt();
-		int[] a = new int[n];
+		int size = scan.nextInt();
+		int[] array = new int[size];
 		System.out.println("Enter the elements with a missing number: ");
-		for(int i=0; i<a.length; i++) {
-			a[i] = scan.nextInt();
+		for(int i=0; i<array.length; i++) {
+			array[i] = scan.nextInt();
 		}
-		System.out.println(missingNumber(a));
+		System.out.println(missingNumber(array));
 		scan.close();
 	}
-	public static int missingNumber(int[] a) {
-		int n = a.length + 1;
+	public static int missingNumber(int[] array) {
+		int n = array.length + 1;
 		int sum1 = n * (n+1)/2;
 		int sum2=0;
-		for(int i=0; i<a.length; i++) {
-			sum2+=a[i];
+		for(int i=0; i<array.length; i++) {
+			sum2+=array[i];
 		}
 		return sum1-sum2;
 	}
