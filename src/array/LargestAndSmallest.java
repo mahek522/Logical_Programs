83f3e6999e7f5948272b00308b28226c196a1dd8
Mahek Sultana <121860817+mahek522@users.noreply.github.com>
2025-12-26 17:37:30 +0530

    Renaming the variable names

--- src/array/LargestAndSmallest.java
+++ src/array/LargestAndSmallest.java
@@ -7,25 +7,25 @@ public class LargestAndSmallest {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Enter the size of array");
-		int n = scan.nextInt();
-		int[] a = new int[n];
+		int size = scan.nextInt();
+		int[] array = new int[size];
 		System.out.println("Enter the array elements: ");
-		for(int i=0; i<a.length; i++) {
-			a[i] = scan.nextInt();
+		for(int i=0; i<array.length; i++) {
+			array[i] = scan.nextInt();
 		}
-		largestSmallest(a);
+		largestSmallest(array);
 		scan.close();
 	}
-	public static void largestSmallest(int[] a) {
+	public static void largestSmallest(int[] array) {
 		int largest = Integer.MIN_VALUE;
 		int smallest = Integer.MAX_VALUE;
-		for(int i=0; i<a.length; i++) {
-			if(a[i]>largest) {
+		for(int i=0; i<array.length; i++) {
+			if(array[i]>largest) {
 				largest = a[i];
 			}
 		}
-		for(int i=0; i<a.length; i++) {
-			if(a[i]<smallest) {
+		for(int i=0; i<array.length; i++) {
+			if(array[i]<smallest) {
 				smallest = a[i];
 			}
 		}
