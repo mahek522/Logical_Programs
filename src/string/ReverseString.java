0aca9bbae56949154849ec7112b5b80e086c8533
Mahek Sultana <121860817+mahek522@users.noreply.github.com>
2025-12-26 17:52:27 +0530

    Renaming variable names

--- src/string/ReverseString.java
+++ src/string/ReverseString.java
@@ -8,17 +8,17 @@ public class ReverseString {
 
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Enter the String to be reversed:  ");
-		String s = scan.nextLine();
+		String string = scan.nextLine();
 		System.out.println("Reversed String: ");
-		System.out.println(reverse(s));
+		System.out.println(reverse(string));
 		scan.close();
 	}
-	public static String reverse(String s) {
-		String t = "";
-		for(int i=s.length()-1; i>=0; i--) {
-			t = t + s.charAt(i);
+	public static String reverse(String string) {
+		String temp = "";
+		for(int i=string.length()-1; i>=0; i--) {
+			temp = temp + string.charAt(i);
 		}
-		return t;
+		return temp;
 	}
 
 }
